0ed9744599fa2be2ea69c0bbd3d08f929ddb64da
amittras-pal <pal.amittras@gmail.com>
2025-01-26 19:07:13 +0530

    Fix text color on NoDataOverlay.

--- src/components/ag-grid/plugins/overlays.tsx
+++ src/components/ag-grid/plugins/overlays.tsx
@@ -1,9 +1,8 @@
-import { Box, Text, ThemeIcon, useMantineTheme } from "@mantine/core";
+import { Box, Text, ThemeIcon } from "@mantine/core";
 import { IconTemplate } from "@tabler/icons-react";
 import { NoDataOverlayProps } from "../interfaces";
 
 export function NoDataOverlay(props: Readonly<NoDataOverlayProps>) {
-  const { primaryColor } = useMantineTheme();
   return (
     <Box
       sx={{
@@ -19,7 +18,12 @@ export function NoDataOverlay(props: Readonly<NoDataOverlayProps>) {
       <ThemeIcon size={140} variant="light" radius="lg">
         <IconTemplate size={100} />
       </ThemeIcon>
-      <Text ta="center" sx={() => ({ color: primaryColor[4] })}>
+      <Text
+        ta="center"
+        sx={(theme) => ({
+          color: theme.colors[theme.primaryColor][3],
+        })}
+      >
         {props.message}
       </Text>
     </Box>
