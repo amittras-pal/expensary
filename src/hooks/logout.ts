dd10caf81c27727b04997d2c715b89eea20d7d70
Amittras Pal <pal.amittras@gmail.com>
2025-08-15 12:59:32 +0530

    Migrate session management to HttpOnly Cookie
    - Remove request interceptor to append bearer token.
    - Update error handler to logout on Unauthorized response.
    - Add function and endpoint to logout user.

--- src/hooks/logout.ts
+++ src/hooks/logout.ts
@@ -1,20 +1,24 @@
 import { useLocalStorage } from "@mantine/hooks";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { primaryColor } from "../constants/app";
+import { logoutUser } from "../services/user.service";
 
 export const useLogoutHandler = () => {
   const navigate = useNavigate();
   const client = useQueryClient();
   const [, setPrimaryColor] = useLocalStorage({ key: "primary-color" });
 
-  const logoutUser = () => {
-    localStorage.clear();
-    client.clear();
-    window.dispatchEvent(new Event("storage"));
-    navigate("/login");
-    setPrimaryColor(primaryColor);
-  };
+  const { mutate: logout, isLoading: loggingOut } = useMutation({
+    mutationFn: logoutUser,
+    onSuccess: () => {
+      localStorage.clear();
+      client.clear();
+      window.dispatchEvent(new Event("storage"));
+      navigate("/login");
+      setPrimaryColor(primaryColor);
+    },
+  });
 
-  return { logoutUser };
+  return { logout, loggingOut };
 };
