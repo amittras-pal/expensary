bf969306a6ff995271a9e551e4c1f6af8b9a8f68
Amittras Pal <72859660+amittras-pal@users.noreply.github.com>
2026-02-20 04:30:35 +0000

    Add AccountInfo component for user account details management

--- src/modules/user/index.tsx
+++ src/modules/user/index.tsx
@@ -1,16 +1,13 @@
-import { Container, Divider, Group, Paper, Text, Title } from "@mantine/core";
+import { Container, Paper, Title } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
-import dayjs from "dayjs";
 import { APP_TITLE } from "../../constants/app";
-import { useCurrentUser } from "../../context/user.context";
 import classes from "../../theme/modules/account.module.scss";
+import AccountInfo from "./AccountInfo";
 import ChangePassword from "./ChangePassword";
 import Preferences from "./Preferences";
 
 export default function User() {
   useDocumentTitle(`${APP_TITLE} | My Account`);
-
-  const { userData } = useCurrentUser();
 
   return (
     <Container
@@ -20,34 +17,9 @@ export default function User() {
     >
       <Paper className={classes.tile}>
         <Title order={4} mb="xs">
-          Account Info
-        </Title>
-        <Text fz="2rem" fw="bold" fs="italic" mb="xs">
-          {userData?.userName}
-        </Text>
-        <Group gap="sm" wrap="wrap">
-          <Text component="p" m={0} fz="xs">
-            <Text component="span" c="dimmed">
-              Member Since:{" "}
-            </Text>
-            <Text component="span" fw="bold">
-              {dayjs(userData?.createdAt).format("DD MMM 'YY")}
-            </Text>
-          </Text>
-          {userData?.createdAt !== userData?.updatedAt && (
-            <>
-              <Divider orientation="vertical" color={userData?.color} />
-              <Text component="p" m={0} fz="xs">
-                <Text component="span" c="dimmed">
-                  Last Updated:{" "}
-                </Text>
-                <Text component="span" fw="bold">
-                  {dayjs(userData?.updatedAt).format("DD MMM' YY")}
-                </Text>
-              </Text>
-            </>
-          )}
-        </Group>
+                Account Info
+              </Title>
+      <AccountInfo />
       </Paper>
       <Paper className={classes.tile}>
         <Title order={3} mb="xs">
